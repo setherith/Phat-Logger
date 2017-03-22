1c9d2e13ea334466fc04e692355306351e1bd792
Shane Pudner <setherith@gmail.com>
2017-03-22 18:58:37 +0000

    Adding jquery and tidy up on main page

--- web/index.js
+++ web/index.js
@@ -8,6 +8,7 @@ app.set('view engine', 'pug');
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
 
 app.get('/', function (req, res) {
 	res.render('index.pug');
