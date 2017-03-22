35f347483ff3703b37520b860df9fc27d6836e9c
Shane Pudner <setherith@gmail.com>
2017-03-22 20:07:34 +0000

    Started working on Chart.js integration

--- web/index.js
+++ web/index.js
@@ -15,16 +15,19 @@ app.set('view engine', 'pug');
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/chart', express.static(__dirname + '/node_modules/chart.js/dist'));
 
 app.get('/', function (req, res) {
 	conn.query('select * from log', function(err, rows) {
 		if (err) console.log(err);
 		var output = '';
+		var raw = '[';
 		rows.forEach(function(r) {
 			output += r['datetime'] + ':' + r['value'] + '\n';
+			raw += r['value'] + ', ';
 		});
-		res.render('index.pug', { 'rows': output });
+		res.render('index.pug', { 'rows': output, 'raw': raw + ']' });
 	});
 });
 
