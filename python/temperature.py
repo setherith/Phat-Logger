3663b2a2e62bc56b73c6a129f39840e6300f54ed
Shane Pudner <setherith@gmail.com>
2017-04-14 23:30:30 +0100

    Added module (python) for temperature

---
+++ python/temperature.py
@@ -0,0 +1,16 @@
+from envirophat import weather
+import time
+import pymysql
+
+conn = pymysql.connect(host='localhost', database='phat-logger', user='phat_user', passwd='phat_user')
+conn.autocommit(True)
+
+cur = conn.cursor()
+
+while(True):
+	temp = weather.temperature()
+	print("Logging: " + '%.5f' % temp)
+	cur.execute('call log(' + '%.5f' % temp + ')')
+	time.sleep(10)
+cur.close()
+conn.close()
