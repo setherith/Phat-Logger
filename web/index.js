abb30a9ae799faf308eade9b5be6333d9f29584f
Shane Pudner <setherith@gmail.com>
2017-04-16 01:14:00 +0100

    Shortened labels and adjusted time span to 30 minutes

--- web/index.js
+++ web/index.js
@@ -19,7 +19,7 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/chart', express.static(__dirname + '/node_modules/chart.js/dist'));
 
 app.get('/', function (req, res) {
-	conn.query('select * from log', function(err, rows) {
+	conn.query('select * from log where datetime > sysdate() - interval 30 minute', function(err, rows) {
 		if (err) console.log(err);
 		var output = '';
 		var values = '[';
@@ -27,7 +27,7 @@ app.get('/', function (req, res) {
 		rows.forEach(function(r) {
 			output += r['datetime'] + ':' + r['value'] + '\n';
 			values += r['value'] + ', ';
-			labels += "'" + r['datetime'] + "', ";
+			labels += "'" + toDate(r['datetime']) + "', ";
 		});
 		res.render('index.pug', { 'rows': output, 
 					'values': values + ']',
@@ -38,3 +38,12 @@ app.get('/', function (req, res) {
 app.listen(3000, function() {
 	console.log('Web App started @ ' + new Date() + '\nConnect -> localhost:3000');
 });
+
+function toDate(dbdate) {
+	var date = new Date(dbdate);
+	var hour = date.getHours();
+	var min = date.getMinutes();
+	if (hour.toString().length < 2) hour = '0' + hour;
+	if (min.toString().length < 2) min = '0' + min;
+	return hour + ':' + min;
+}
